f85995571ba579aeaba6a97236fc89b467655b4c
Jeferson <jefersongabri@gmail.com>
2023-06-06 18:43:48 -0300

    Acho que achei o erro

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const AuthController = require('../controllers/AuthController');
+const authController = require('../controllers/authController');
 
 // helpers
 const checkOut = require('../helpers/out').checkOut;
 
 // GET
-router.get('/', checkOut, AuthController.login);
-router.get('/logout', AuthController.logout);
+router.get('/', checkOut, authController.login);
+router.get('/logout', authController.logout);
 
 // POST
-router.post('/register', checkOut, AuthController.registerPost);
-router.post('/login', checkOut, AuthController.loginPost);
+router.post('/register', checkOut, authController.registerPost);
+router.post('/login', checkOut, authController.loginPost);
 
 module.exports = router;
